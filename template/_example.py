72f3fad98d2d32e3fa224500c38b5d2dc43a1f05
rochero <rochero@163.com>
2023-02-17 20:00:02 +0800

    flatten some code

--- template/_example.py
+++ template/_example.py
@@ -6,10 +6,11 @@ import traceback
 import logging
 import random
 import time
-from threading import Lock, Thread, Event
+from threading import Lock, Thread
 from queue import Queue
 from requests.packages import urllib3
-from requests_html import HTMLSession, HTMLResponse
+from requests import Session, Response
+from parsel import Selector
 from concurrent.futures import ThreadPoolExecutor
 from tqdm import tqdm
 from utils.tool import save_raw, save_txt, save_xlsx, save_csv
@@ -19,8 +20,11 @@ urllib3.disable_warnings()
 # lock for writing logs
 lock = Lock()
 
+# sign of the end of scraping
+_end = object()
+
 # store response waiting for parse
-q = Queue()
+q = Queue(1000)
 
 # session pool for reuse
 sq = Queue()
@@ -33,24 +37,26 @@ cfg = {
     # max retry count per request
     'retry': 2,
     # max consecutive request failed count
-    'max_failed_count': 5,
+    'max_failed_count': 100,
     # min wait seconds for next retry request
     'wait': 3,
     # request delay interval (min, max)
-    'interval': (0.1, 0.2),
+    'interval': (0.1, 0.1),
     'parse_worker_num': 16,
     'log': './log/log.txt',
-    'error_while_parsing': './log/err.txt',
-    'error_while_scraping': './log/lost.txt',
+    'parse_error': './log/err.txt',
+    'scrape_error': './log/lost.txt',
     '404': './log/404.txt',
     'headers': {
         'accept': '*/*',
+        'origin': 'https://www.google.com',
+        'referer': 'https://www.google.com/',
         'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36 Edg/108.0.1462.46'
     },
     'proxies': {'http': '127.0.0.1:2334', 'https': '127.0.0.1:2334'},
 }
 
-logging.basicConfig(filename=cfg['log'], level=logging.DEBUG,
+logging.basicConfig(filename=cfg['log'], level=logging.INFO,
                     format='%(asctime)s - %(levelname)s - %(message)s')
 
 
@@ -62,13 +68,11 @@ def get_data(pref, session=None):
         try:
             session = sq.get(timeout=20)
         except Exception:
-            session = HTMLSession()
+            session = Session()
     headers = cfg.get('headers')
-    try_cnt = cfg.get('retry') if cfg.get(
-        'retry') and cfg.get('retry') > 0 else 3
-    wait_time = cfg.get('wait') if cfg.get(
-        'wait') and cfg.get('wait') > 0 else 3.0
-    while try_cnt >= 0:
+    retry_cnt = cfg.get('retry') or 2
+    wait_time = cfg.get('wait') or 2
+    while retry_cnt >= 0:
         try:
             res = session.get(
                 url,
@@ -77,100 +81,66 @@ def get_data(pref, session=None):
                 verify=False,
                 proxies=cfg.get('proxies')
             )
-        except Exception:
-            time.sleep(wait_time*random.uniform(1, 2))
-            try_cnt -= 1
-            continue
-        if is_right(res):
-            sq.put(session)
-            failed_cnt = 0
-            return res
-        else:
-            if no_retry(res):
-                break
-            time.sleep(wait_time*random.uniform(1, 2))
-            try_cnt -= 1
-    lock.acquire(10)
+            if is_right(res):
+                sq.put(session)
+                failed_cnt -= 1 if failed_cnt else 0
+                return res
+        except:
+            logging.info(traceback.format_exc(limit=3))
+        if no_retry(res):
+            break
+        time.sleep(wait_time*random.uniform(1, 2))
+        retry_cnt -= 1
+    lock.acquire()
     try:
-        if not_found(res):
-            print('not found: {}'.format(url))
-            save_txt([[url]], cfg['404'])
-        else:
-            print('request failed: {}'.format(url))
-            save_txt([[url]], cfg['error_while_scraping'])
+        print('request failed: {}'.format(url))
+        save_txt([[url,res.status_code]], cfg['scrape_error'])
     finally:
         lock.release()
         failed_cnt += 1
         if failed_cnt > cfg['max_failed_count']:
             Beep(440, 1000)
-            exit(1)
+            exit(0)
         sq.put(session)
     return None
 
 
-def is_right(res):
-    return res and (res.status_code == 200 or res.status_code == 304)
+def is_right(res: Response):
+    return res and (res.status_code//100 == 2 or res.status_code//100 == 3)
 
 
-def no_retry(res):
+def no_retry(res: Response):
     return res and (res.status_code == 404 or res.status_code == 503)
 
 
-def not_found(res):
+def not_found(res: Response):
     return res and res.status_code == 404
 
 
-def parse_data(r: HTMLResponse):
+def parse_data(r: Response):
     if not r:
-        raise Exception('no data')
+        return None
+    if not r.content:
+        raise Exception('response content is empty')
     global failed_cnt
     res = []
     title = alt = id = date = year = genre = theme = sys = tags = desc = dev = pub = size = score = votes = 0
     try:
-        s = r.html
+        s = Selector(r.text)
+        # j = r.json()['content']
         if s:
-            title = s.xpath(
-                '//a[@itemprop="mainEntityOfPage"]/text()', first=True)
-            dev = s.xpath(
-                '//span[@itemprop="author"]//span[@itemprop="name"]/text()', first=True)
-            pub = s.xpath(
-                '//h5[contains(text(),"Publisher")]/following-sibling::span[1]/a/text()')
-            pub = '|'.join(pub) if pub else ''
-            date = s.xpath(
-                '//time[@itemprop="datePublished"]/@datetime', first=True)
-            desc = s.xpath('//p[@itemprop="description"]/text()', first=True)
-            sys = s.xpath('//span[@itemprop="operatingSystem"]/a/text()')
-            sys = '|'.join(sys) if sys else ''
-            score = s.xpath(
-                '//meta[@itemprop="ratingValue"]/@content', first=True)
-            votes = s.xpath(
-                '//span[@itemprop="ratingCount"]/text()', first=True)
-            tags = s.xpath(
-                '//div[@id="tagsform"]//a[contains(@href, "/tags/")]/text()')
-            tags = '|'.join(tags) if tags else ''
-            genre = s.xpath('//span[@itemprop="genre"]/a/text()')
-            genre = '|'.join(genre) if genre else ''
-            theme = s.xpath(
-                '//h5[contains(text(),"Theme")]/following-sibling::span[1]/a/text()')
-            theme = '|'.join(theme) if theme else ''
-            engine = s.xpath(
-                '//h5[contains(text(),"Engine")]/following-sibling::span[1]/a/text()')
-            engine = '|'.join(engine) if engine else ''
-            players = s.xpath(
-                '//h5[contains(text(),"Player")]/following-sibling::span[1]/a/text()', first=True)
-            project = s.xpath(
-                '//h5[contains(text(),"Project")]/following-sibling::span[1]/a/text()', first=True)
-            return [title, r.url, genre, date, sys, project, engine, theme, tags, desc, dev, pub, players, score, votes]
-        raise Exception('no data')
-    except Exception:
+            s.xpath()
+            return None
+        raise Exception('target data not found')
+    except Exception as e:
         traceback.print_exc(limit=3)
-        lock.acquire(10)
+        lock.acquire()
         try:
-            save_txt([[r.url]], cfg['error_while_parsing'])
+            save_txt([[r.url, e]], cfg['parse_error'])
         finally:
             lock.release()
             failed_cnt += 1
-        logging.debug(traceback.format_exc(limit=3))
+        logging.info(traceback.format_exc(limit=3))
         return None
 
 
@@ -178,35 +148,34 @@ def append_data(d: list, r):
     if not r:
         return
     if isinstance(r, list):
-        if isinstance(r[0], list):
-            d.extend(r)
-        else:
-            d.append(r)
+        d += r if isinstance(r[0], list) else [r]
     else:
         d.append([r])
 
 
+def sleep(interval=None):
+    min, max = interval or cfg.get('interval')
+    if max > min and min > 0:
+        time.sleep(random.uniform(min, max))
+
+
 def scraper(l, i, prefs, max_workers):
-    min_lag, max_lag = cfg.get('interval') if cfg.get('interval') else (0, 0)
     with ThreadPoolExecutor(max_workers) as t:
         for r in tqdm(t.map(get_data, prefs), desc='scraping...{}/{}'.format(i, l), total=len(prefs)):
             q.put(r)
-            if max_lag > min_lag and min_lag > 0:
-                time.sleep(random.uniform(min_lag, max_lag))
-    q.put(object())
+            sleep()
+    q.put(_end)
+    while q.qsize() > 1:
+        print(f'\rparsing...{q.qsize()-1:8d} left', end='', flush=True)
 
 
 def parse_worker(d: list):
     while True:
-        try:
-            r = q.get(timeout=10)
-            if r:
-                if r is object():
-                    q.put(r)
-                    break
-                append_data(d, parse_data(r))
-        except Exception:
-            pass
+        r = q.get()
+        if r is _end:
+            q.put(r)
+            return
+        append_data(d, parse_data(r))
 
 
 def parser(d: list):
@@ -227,26 +196,24 @@ def saver(d: list, save_path: str):
         raise RuntimeError('not saved: unsupported file format')
 
 
-def crawl(u_list=[], save_path: str = '', chunk_size: int = 0, max_workers: int = 1):
-    chunks = [u_list[i:i + chunk_size]
-              for i in range(0, len(u_list), chunk_size)]
-    if max_workers < 2:
-        session = HTMLSession()
-        for chunk in chunks:
-            d_list = []
-            for c in chunk:
-                print(c)
-                append_data(d_list, parse_data(get_data(c, session)))
-                min_lag, max_lag = cfg.get(
-                    'interval') if cfg.get('interval') else (0, 0)
-                if max_lag > min_lag and min_lag > 0:
-                    time.sleep(random.uniform(min_lag, max_lag))
-            saver(d_list, save_path)
-            system('cls')
-        return
+def one_worker_crawl(chunks: list, save_path: str):
+    session = Session()
+    for chunk in chunks:
+        d_list = []
+        for c in chunk:
+            print(c)
+            append_data(d_list, parse_data(get_data(c, session)))
+            sleep()
+        saver(d_list, save_path)
+        system('cls')
+
+
+def multi_workers_crawl(chunks: list, save_path, max_workers: int = 2):
     for _ in range(0, max_workers):
-        sq.put(HTMLSession())
+        sq.put(Session())
     for i, chunk in enumerate(chunks, 1):
+        while not q.empty():
+            q.get()
         # d_list: [[],[]...,[]]
         d_list = []
         t1 = Thread(target=scraper, args=(
@@ -256,3 +223,12 @@ def crawl(u_list=[], save_path: str = '', chunk_size: int = 0, max_workers: int 
         t1.join(), t2.join()
         saver(d_list, save_path)
         system('cls')
+
+
+def crawl(u_list=[], save_path: str = '', chunk_size: int = 0, max_workers: int = 1):
+    chunks = [u_list[i:i + chunk_size]
+              for i in range(0, len(u_list), chunk_size)]
+    if max_workers < 2:
+        one_worker_crawl(chunks, save_path)
+    else:
+        multi_workers_crawl(chunks, save_path)
--- utils/tool.py
+++ utils/tool.py
@@ -2,80 +2,51 @@ import gzip
 import time
 import os
 from os import path, makedirs
-import openpyxl
 import pandas as pd
 import numpy as np
 import time
-from openpyxl.cell.cell import ILLEGAL_CHARACTERS_RE
 
 
-def timeStamp(timeNum):
-    timeStamp = float(timeNum/1000)
+def time_stamp(timeNum, date_only=True):
+    timeStamp = timeNum/1000
     timeArray = time.localtime(timeStamp)
-    otherStyleTime = time.strftime("%Y-%m-%d %H:%M:%S", timeArray)
+    otherStyleTime = time.strftime('%Y-%m-%d'+ '' if date_only else ' %H:%M:%S', timeArray)
     return otherStyleTime
-
 
 def load_txt(load_path, first=False, sep='\t'):
     with open(load_path, 'r', encoding='utf-8', errors='ignore') as f:
         return [l.strip().split(sep)[0] if first else l.strip().split(sep) for l in f.readlines()]
 
 
-def load_pattern(pattern: str, params):
-    return [pattern.format(x) for x in params]
+def make_dir(save_path = None):
+    dir, __ = path.split(save_path)
+    if dir:
+        makedirs(dir, exist_ok=True)
 
 
 def save_txt(data: list, save_path='data.txt', sep='\t', override=False):
-    dir, __ = path.split(save_path)
-    if dir:
-        makedirs(dir, exist_ok=True)
+    if not data: return
+    make_dir(save_path)
     with open(save_path, "w+" if override else "a+", encoding="utf-8", errors="ignore") as f:
-        if data:
-            for d in data:
-                if d:
-                    f.write(sep.join(str(item) for item in d)+'\n')
-
-
-def save_xlsx(data: list, save_path='data.xlsx', table_name='Sheet', backup=False):
-    dir, __ = path.split(save_path)
-    if dir:
-        makedirs(dir, exist_ok=True)
-    try:
-        read_xlsx = openpyxl.load_workbook(save_path)
-    except Exception:
-        read_xlsx = openpyxl.Workbook()
-    if backup:
-        read_xlsx.save('backup_{}.xlsx'.format(int(time.time())))
-    try:
-        table = read_xlsx[table_name]
-    except Exception:
-        read_xlsx.create_sheet(table_name)
-        table = read_xlsx[table_name]
-    nrows = table.max_row
-    if data:
-        for d in data:
-            for j in range(0, len(d)):
-                table.cell(nrows+1, j+1).value = ILLEGAL_CHARACTERS_RE.sub(r'',
-                                                                           d[j]) if isinstance(d[j], str) else d[j]
-            nrows = nrows + 1
-    read_xlsx.save(save_path)
+        f.writelines(sep.join(str(item) for item in d)+'\n' for d in data if any(d))
 
 
 def save_raw(raw, save_path):
-    dir, __ = path.split(save_path)
-    if dir:
-        makedirs(dir, exist_ok=True)
+    make_dir(save_path)
     with open(save_path, 'wb+') as f:
         f.write(raw)
 
 
-def save_csv(data: list, save_path='data.csv', sep=',', override=False):
-    dir, __ = path.split(save_path)
-    if dir:
-        makedirs(dir, exist_ok=True)
-    pd.DataFrame(np.array(data)).to_csv(save_path, sep=sep,
-                                        mode='w' if override else 'a', header=False, index=False)
+def save_csv(data: list, save_path='data.csv', sep=',', override=False, col:list | bool = False):
+    make_dir(save_path)
+    df = pd.DataFrame(np.array(data))
+    df.to_csv(save_path, sep=sep, mode='w' if override else 'a', header = col, index=False)
 
+def save_xlsx(data: list, save_path='data.xlsx', sheet_name='Sheet1', override=False,col:list | bool = False):
+    make_dir(save_path)
+    df = pd.DataFrame(np.array(data))
+    with pd.ExcelWriter(save_path, mode='w' if override or not path.exists(save_path) else 'a', engine='openpyxl') as writer:
+        df.to_excel(writer,sheet_name=sheet_name, header = col, index=False)
 
 def un_gz(file_path, delete_source=False):
     f_name = file_path.replace('.gz', '')
